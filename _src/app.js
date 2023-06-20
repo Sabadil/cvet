b13f9ee4cc88a74c08c8e650257234339f919953
Ivan <zautorin@gmail.com>
2023-06-20 23:18:50 +0300

    first commit

---
+++ _src/app.js
@@ -0,0 +1,13 @@
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
+import './fonts.css';
+import './reset.css';
+import './app.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
